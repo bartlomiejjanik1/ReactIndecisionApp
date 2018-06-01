21ae2fc450373152141faa51be4bff63717bbe0f
Bartek <bartlomiejjanik1@gmail.com>
2018-06-01 13:50:49 +0200

    zmienne przekazane do metody ReactDOM.render()

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,5 +2,6 @@ console.log("app.js is running!");
 
 //JSX - JavaScript XML
 var template = <p>This is JSX z app.js</p>;
+var appRoot = document.getElementById('app');
 
-ReactDOM.render(template);
+ReactDOM.render(template, appRoot);
