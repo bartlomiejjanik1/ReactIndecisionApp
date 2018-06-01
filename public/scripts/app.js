bc72c2c28b9bf355a8f306abb5f56d8e00294284
Bartek <bartlomiejjanik1@gmail.com>
2018-06-01 13:57:34 +0200

    przed instalacja babeljs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,12 @@
 console.log("app.js is running!");
 
 //JSX - JavaScript XML
-var template = <p>This is JSX z app.js</p>;
+//var template = <p>This is JSX z app.js</p>;
+var template = React.createElement(
+    "p",
+    null,
+    "This is JSX z app.js"
+  );
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
