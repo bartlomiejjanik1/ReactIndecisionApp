12ad4ff122e89ac66073e762c0a8fa6ed3860de4
bartlomiejjanik1 <33162089+bartlomiejjanik1@users.noreply.github.com>
2018-06-05 08:47:05 +0200

    Condition rendering

--- src/app.js
+++ src/app.js
@@ -20,14 +20,19 @@ var user = {
     age: 26,
     location: 'Krakow'
 };
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1> 
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
         <p>Age: {user.age}</p>
-        <p> Location: {user.location} </p>
+        {getLocation(user.location)}
     </div>
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
