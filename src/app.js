54a18d73555501558d3cb78dbfcdead32801db19
bartlomiejjanik1 <33162089+bartlomiejjanik1@users.noreply.github.com>
2018-06-05 15:58:33 +0200

    indecision forms

--- src/app.js
+++ src/app.js
@@ -1,42 +1,31 @@
 console.log("app.js is running!");
-//only render subtitle and p tag, if subtitle exist - logical operator
-//render new p tag - if options.length > 0 "Here are options" else "No options"
-var app = {
+
+const app = {
     title: 'Indecision App',
     subtitle: 'Zadania na wczoraj!',
-    options: []
+    options: ['One', 'Two']
 };
-var template = (
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+};
+const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        
         {app.subtitle && <p>{app.subtitle}</p>}
         <p>{app.options.length > 0 ? 'Here are options' : 'No options'}</p>
         <ol>
             <li>pierwsza</li>
             <li>druga</li>
         </ol>
+        <form onSubmit={onFormSubmit}>
+             <input type="text" name="option" />
+             <button>Add Option</button>
+        </form>
     </div>
 );
 
-var user = {
-    name: 'Bartek',
-    age: 28,
-    location: 'Krakow'
-};
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
-    }
-}
-var templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {(user.age && user.age >= 18) &&  <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
-
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
